b015dbc168f1c2534cd98bbea6f12a9d7ef274b8
ridzik-sandbox <stephenridzik@Stephens-MBP.fios-router.home>
2024-03-06 09:27:34 -0500

    moving themeprovider to index

--- client/index.js
+++ client/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-import App from './App';
+import App from './src/App';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
 });
 
+const defaultTheme = createTheme();
+
 const container = document.getElementById('app');
 const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <ThemeProvider theme={defaultTheme}>
+        <App />
+      </ThemeProvider>
     </ApolloProvider>
   </React.StrictMode>
 );
