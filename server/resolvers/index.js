9b31fbc2750698371fd1cb58e8916350aa294826
ridzik-sandbox <stephenridzik@Stephens-MBP.fios-router.home>
2024-03-07 00:27:10 -0500

    cleanup

--- server/resolvers/index.js
+++ server/resolvers/index.js
@@ -5,17 +5,10 @@ const resolvers = {
     skaters: SkatersResolver.getSkaters,
     skater: SkatersResolver.getSkater,
   },
-  // Mutation: {
-  //   registerUser: UserResolver.registerUser,
-  //   loginUser: UserResolver.loginUser,
-  //   createPost: PostsResolver.createPost,
-  //   deletePost: PostsResolver.deletePost,
-  //   updatePost: PostsResolver.updatePost,
-  //   createComment: CommentsResolver.createComment,
-  //   deleteComment: CommentsResolver.deleteComment,
-  //   addLike: LikesResolver.addLike,
-  //   removeLike: LikesResolver.removeLike,
-  // }
+  Mutation: {
+    // addLike: LikesResolver.addLike,
+    // removeLike: LikesResolver.removeLike,
+  },
 };
 
 module.exports = { resolvers };
