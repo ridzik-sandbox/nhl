28e61f6e3dcab011929c1408d86697a78e5ed0c7
ridzik-sandbox <stephenridzik@Stephens-MBP.fios-router.home>
2024-03-06 19:51:11 -0500

    pulling theme for now, returning to light theme

--- client/src/styles/theme.js
+++ client/src/styles/theme.js
@@ -8,43 +8,43 @@ import grey from '@mui/material/colors/grey';
 const theme = (mode) =>
   createTheme({
     palette: {
-      mode: 'dark',
-      primary: {
-        main: '#90caf9',
-      },
-      secondary: {
-        main: '#ce93d8',
-      },
-      background: {
-        default: '#121212',
-        paper: '#121212',
-      },
-      error: {
-        light: red[400],
-        main: red[500],
-        dark: red[300],
-        contrastText: grey[800],
-      },
-      success: {
-        main: green[500],
-      },
-      warning: {
-        main: yellow[500],
-        contrastText: grey[800],
-      },
-      info: {
-        main: lightBlue[500],
-      },
-      text: {
-        primary: '#fff',
-        secondary: 'rgba(255, 255, 255, 0.7)',
-        disabled: 'rgba(255, 255, 255, 0.5)',
-      },
-      common: {
-        black: grey[900],
-        white: grey[200],
-      },
-      tonalOffset: 0.2,
+      mode: 'light',
+      // primary: {
+      //   main: '#90caf9',
+      // },
+      // secondary: {
+      //   main: '#ce93d8',
+      // },
+      // background: {
+      //   default: '#121212',
+      //   paper: '#121212',
+      // },
+      // error: {
+      //   light: red[400],
+      //   main: red[500],
+      //   dark: red[300],
+      //   contrastText: grey[800],
+      // },
+      // success: {
+      //   main: green[500],
+      // },
+      // warning: {
+      //   main: yellow[500],
+      //   contrastText: grey[800],
+      // },
+      // info: {
+      //   main: lightBlue[500],
+      // },
+      // text: {
+      //   primary: '#fff',
+      //   secondary: 'rgba(255, 255, 255, 0.7)',
+      //   disabled: 'rgba(255, 255, 255, 0.5)',
+      // },
+      // common: {
+      //   black: grey[900],
+      //   white: grey[200],
+      // },
+      // tonalOffset: 0.2,
     },
   });
 
